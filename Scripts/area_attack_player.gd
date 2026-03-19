1dc5b95bea9f9528378c3708b68a1a4fd25cb173
AdrianoNeyra <adrianoneyra2007@gmail.com>
2026-03-19 11:31:41 +0000

    Cambios jugador

--- Scripts/area_attack_player.gd
+++ Scripts/area_attack_player.gd
@@ -1,22 +1,19 @@
 extends Area2D
 
-var animated_sprite : AnimatedSprite2D
-var player : CharacterBody2D
-@onready var attack_r: CollisionShape2D = $attackR
-@onready var attack_l: CollisionShape2D = $attackL
+var sprite : Sprite2D
+var pivot : Node2D
+var are
 
 func _ready() -> void:
-	player = get_parent()
-	animated_sprite = get_node("../AnimatedSprite2D")
+	
+	sprite = get_node("../../Sprite2D")
+	pivot = get_parent()
 
 func _process(delta: float) -> void:
-	pass
+	update_area()
 
 func update_area():
-	attack_r.disabled = true
-	attack_l.disabled = true
-	if player.current_state == ATTACK_MELEE:
-		if not animated_sprite.flip_h:
-			attack_r.disabled = false
-		else:
-			attack_l.disabled = false
+	if not sprite.flip_h:
+		pivot.rotation_degrees = 0
+	else:
+		pivot.rotation_degrees = 180
